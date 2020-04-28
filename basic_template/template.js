219d359cb3bc2cd53c989ee4a385e75bbc3ebeff
neternefer <alkarinqe@yahoo.com>
2020-04-28 18:48:43 +0200

    Add input check function.

--- basic_template/template.js
+++ basic_template/template.js
@@ -14,4 +14,43 @@ checkInput = (input) => {
     };
 };
 
-document.querySelector(input[type='email']).addEventListener('blur', checkInput);
+//Check form - after every form change
+checkForm = (form) => {
+    const inputs = Array.prototype.slice.apply(form.elements);
+    const formErrors = inputs.map((i) => {
+        if(!i.checkValidity) {
+            checkInput(i);
+        }
+        return i.checkValidity()
+    });
+    console.log('formErrors: ', formErrors)
+    return formErrors;
+};
+
+//Check if submit possible
+checkSubmit = (lst, btn) => {
+    console.log('checkSubmit lst arg: ', lst)
+    if(lst.every(x => x)) {
+        btn.classList.add('active');
+        btn.disabled = false;
+        console.log('Submitted!')
+    }
+    console.log('not submitted')
+};
+
+//Show errors
+//Hide errors
+//Main function - scope?
+checkAll = () => {
+    console.log('checkAll')
+    const formToCheck = document.querySelector('form');
+    const submitBtn = document.querySelector('#submit');
+    const validityLst = checkForm(formToCheck);
+    console.log('ValidityLst: ', validityLst)
+    checkSubmit(validityLst, submitBtn);
+};
+
+    document.querySelector('#email').addEventListener('blur', checkInput);
+    document.querySelector('#submit').addEventListener('click', checkAll);
+
+
